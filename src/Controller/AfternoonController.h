f2df0418d1d21049b694b57ba103c4888d034b7f
Ethan throndsen <Ethunderson1@gmail.com>
2016-01-19 12:42:04 -0700

    added Header Fil

---
+++ src/Controller/AfternoonController.h
@@ -0,0 +1,27 @@
+/*
+ * AfternoonController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: ethr5627
+ */
+
+#ifndef CONTROLLER_AFTERNOONCONTROLLER_H_
+#define CONTROLLER_AFTERNOONCONTROLLER_H_
+
+class AfternoonController
+{
+private:
+	int classCount;
+public:
+	AfternoonController();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+
+
+
+};
+
+
+
+#endif /* CONTROLLER_AFTERNOONCONTROLLER_H_ */
